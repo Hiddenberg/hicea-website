7f690950e164171457fa72cecf9a4a4ef8e20461
hiddenberg <victor_hals@hotmail.com>
2026-01-16 18:41:17 -0600

    creating our focus section

--- application/src/app/page.tsx
+++ application/src/app/page.tsx
@@ -1,6 +1,7 @@
 import MobileHeader from "./components/MobileHeader";
 import AboutUsSection from "./components/sections/AboutUsSection";
 import HeroSection from "./components/sections/HeroSection";
+import OurFocusSection from "./components/sections/OurFocusSection";
 import OurTeamSection from "./components/sections/OurTeamSection";
 
 export default function Home() {
@@ -10,6 +11,7 @@ export default function Home() {
          <HeroSection />
          <AboutUsSection />
          <OurTeamSection />
+         <OurFocusSection />
       </main>
    );
 }
