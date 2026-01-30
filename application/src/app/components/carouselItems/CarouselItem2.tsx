61c3ed920ed09723894f7f2e80ddbd958f225c0e
hiddenberg <victor_hals@hotmail.com>
2026-01-30 08:19:43 -0600

    changing hero button font weight

--- application/src/app/components/carouselItems/CarouselItem2.tsx
+++ application/src/app/components/carouselItems/CarouselItem2.tsx
@@ -3,13 +3,13 @@ import CalendlyButton from "../CalendlyButton";
 export default function CarouselItem2() {
    return (
       <div className="flex justify-center items-center min-w-full">
-         <div className="flex flex-col items-end pb-10 font-bold">
-            <h1 className="text-white text-3xl lg:text-5xl text-right leading-7 lg:leading-10">
+         <div className="flex flex-col items-end pb-10">
+            <h1 className="font-bold text-white text-3xl lg:text-5xl text-right leading-7 lg:leading-10">
                <p>INICIA EL 2026</p>
                <p>CUIDANDOTE</p>
             </h1>
-            <p className="text-hicea-pink text-6xl text-center">CHEK-UP</p>
-            <p className="text-white text-8xl text-center leading-20">$499</p>
+            <p className="font-bold text-hicea-pink text-6xl text-center">CHEK-UP</p>
+            <p className="font-bold text-white text-8xl text-center leading-20">$499</p>
             <div className="flex lg:justify-end items-center px-2 w-full">
                <div className="flex flex-col items-end gap-2">
                   <p className="font-semibold text-hicea-pink text-sm">
