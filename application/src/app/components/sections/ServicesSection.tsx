e86589c87bfeff272f87f2cda5270e9fc95952a3
hiddenberg <victor_hals@hotmail.com>
2026-02-24 09:12:08 -0600

    removing additional service tags

--- application/src/app/components/sections/ServicesSection.tsx
+++ application/src/app/components/sections/ServicesSection.tsx
@@ -42,50 +42,50 @@ function ServiceCard({
    );
 }
 
-function LabStudiesCard() {
-   return (
-      <div className="group lg:flex lg:flex-col shadow-2xl hover:shadow-[0_20px_40px_-12px_rgba(168,85,247,0.2)] transition-all hover:-translate-y-1 duration-300 ease-out">
-         <div className="flex items-center gap-4 bg-hicea-purple p-4 h-22 transition-colors duration-300">
-            <img
-               src={"/assets/estudios.svg"}
-               alt={"Estudios de laboratorio"}
-               className="size-12 object-contain group-hover:scale-105 transition-transform duration-300"
-            />
-            <h3 className="font-semibold text-white text-2xl">
-               Estudios de laboratorio
-            </h3>
-         </div>
-         <div className="flex flex-col justify-between bg-white p-8 lg:grow">
-            <div>
-               <div className="font-bold">
-                  <p className="text-hicea-purple text-4xl leading-8">
-                     REALIZATE TUS
-                  </p>
-                  <p className="text-hicea-pink text-4xl leading-8">
-                     ESTUDIOS MÉDICOS
-                  </p>
-               </div>
+// function LabStudiesCard() {
+//    return (
+//       <div className="group lg:flex lg:flex-col shadow-2xl hover:shadow-[0_20px_40px_-12px_rgba(168,85,247,0.2)] transition-all hover:-translate-y-1 duration-300 ease-out">
+//          <div className="flex items-center gap-4 bg-hicea-purple p-4 h-22 transition-colors duration-300">
+//             <img
+//                src={"/assets/estudios.svg"}
+//                alt={"Estudios de laboratorio"}
+//                className="size-12 object-contain group-hover:scale-105 transition-transform duration-300"
+//             />
+//             <h3 className="font-semibold text-white text-2xl">
+//                Estudios de laboratorio
+//             </h3>
+//          </div>
+//          <div className="flex flex-col justify-between bg-white p-8 lg:grow">
+//             <div>
+//                <div className="font-bold">
+//                   <p className="text-hicea-purple text-4xl leading-8">
+//                      REALIZATE TUS
+//                   </p>
+//                   <p className="text-hicea-pink text-4xl leading-8">
+//                      ESTUDIOS MÉDICOS
+//                   </p>
+//                </div>
 
-               <div className="my-6">
-                  <span className="text-hicea-pink text-xk">
-                     Estudios de laboratorio
-                  </span>
-                  <ul className="text-hicea-purple text-sm leading-tight list-disc list-inside">
-                     <li>Biométrica hematica</li>
-                     <li>Química Sanguínea</li>
-                     <li>Perfil de lípidos,</li>
-                     <li>Perfil ovario</li>
-                     <li>Examen general de orina</li>
-                     <li>y mas.</li>
-                  </ul>
-               </div>
-            </div>
+//                <div className="my-6">
+//                   <span className="text-hicea-pink text-xk">
+//                      Estudios de laboratorio
+//                   </span>
+//                   <ul className="text-hicea-purple text-sm leading-tight list-disc list-inside">
+//                      <li>Biométrica hematica</li>
+//                      <li>Química Sanguínea</li>
+//                      <li>Perfil de lípidos,</li>
+//                      <li>Perfil ovario</li>
+//                      <li>Examen general de orina</li>
+//                      <li>y mas.</li>
+//                   </ul>
+//                </div>
+//             </div>
 
-            <CalendlyButton />
-         </div>
-      </div>
-   );
-}
+//             <CalendlyButton />
+//          </div>
+//       </div>
+//    );
+// }
 
 const serviceCards: ServiceCardProps[] = [
    {
@@ -103,22 +103,22 @@ const serviceCards: ServiceCardProps[] = [
          </div>
       ),
    },
-   {
-      subtitle: "Control prenatal",
-      includesText: "Consulta especializada y ultrasonido obstretico",
-      iconURL: "/assets/control-prenatal.svg",
-      titleComponent: (
-         <div className="font-bold">
-            <p className="text-hicea-purple text-4xl leading-8">
-               CUIDA A TU BEBE
-            </p>
-            <p className="text-hicea-pink text-4xl leading-8">
-               DESDE EL EMBARAZO
-            </p>
-            <p className="text-hicea-pink text-7xl">$650</p>
-         </div>
-      ),
-   },
+   // {
+   //    subtitle: "Control prenatal",
+   //    includesText: "Consulta especializada y ultrasonido obstretico",
+   //    iconURL: "/assets/control-prenatal.svg",
+   //    titleComponent: (
+   //       <div className="font-bold">
+   //          <p className="text-hicea-purple text-4xl leading-8">
+   //             CUIDA A TU BEBE
+   //          </p>
+   //          <p className="text-hicea-pink text-4xl leading-8">
+   //             DESDE EL EMBARAZO
+   //          </p>
+   //          <p className="text-hicea-pink text-7xl">$650</p>
+   //       </div>
+   //    ),
+   // },
 ];
 
 export default function ServicesSection() {
@@ -133,7 +133,7 @@ export default function ServicesSection() {
             {serviceCards.map((service) => (
                <ServiceCard key={service.subtitle} {...service} />
             ))}
-            <LabStudiesCard />
+            {/* <LabStudiesCard /> */}
          </div>
       </div>
    );
